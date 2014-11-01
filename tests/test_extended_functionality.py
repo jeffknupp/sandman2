1f142cf73a14e50e11f26d6be2b8ac101504f1b9
Jeff Knupp <jeff@jeffknupp.com>
2014-11-01 12:04:46 -0400

    Clean up tests; add tests for user-defined models and pagination

---
+++ tests/test_extended_functionality.py
@@ -0,0 +1,12 @@
+"""Tests for non-core functionality in sandman2."""
+
+from pytest_flask.fixtures import client
+
+exclude_tables = ('Invoice')
+
+def test_pagination(client):
+    """Do we return paginated results when a 'page' parameter is provided?"""
+    response = client.get('/artist?page=2')
+    assert response.status_code == 200
+    assert len(response.json['resources']) == 20
+    assert response.json['resources'][0]['ArtistId'] == 21
