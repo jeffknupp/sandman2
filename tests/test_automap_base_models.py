188d7fb92a61a4cf7e716917fb3ae9cb36c07769
Joshua Carp <jm.carp@gmail.com>
2015-08-09 20:55:37 -0400

    Fix failing admin test.

--- tests/test_automap_base_models.py
+++ tests/test_automap_base_models.py
@@ -1,6 +1,7 @@
 """Test using user-defined models with sandman2."""
 import json
 
+from flask import url_for
 from pytest_flask.fixtures import client
 
 from tests.resources import (
@@ -14,6 +15,6 @@ database = 'blog.sqlite3'
 
 def test_get_automap_collection(client):
     """Do we see a model's __unicode__ definition being used in the admin?"""
-    response = client.get('/admin/blogview/')
+    response = client.get(url_for('blog.index_view'))
     assert response.status_code == 200
-    assert 'Jeff Knupp' in response.data
+    assert 'Jeff Knupp' in response.get_data(as_text=True)
