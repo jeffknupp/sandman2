56fda37fe3b15bd92c953811ce79e453d105e728
Sebastian Stein <seb.kde@hpfsc.de>
2016-11-09 14:30:13 +0100

    make admin UI work again fixing https://github.com/jeffknupp/sandman2/issues/50

--- sandman2/admin.py
+++ sandman2/admin.py
@@ -8,7 +8,4 @@ from flask_admin.contrib.sqla import ModelView
 
 class CustomAdminView(ModelView):  # pylint: disable=no-init
     """Define custom templates for each view."""
-    list_template = 'list.html'
-    create_template = 'create.html'
-    edit_template = 'edit.html'
     column_display_pk = True
