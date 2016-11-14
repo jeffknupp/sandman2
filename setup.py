47ac844be564f1390fc5177557b1907b65e7f453
Jeff Knupp <jeff.knupp@engima.io>
2016-11-14 03:56:15 -0500

    Include MANIFEST.in so template files get included in sdist (fixes #50)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name='sandman2',
-    version='1.0.4',
+    version='1.0.5',
     url='http://github.com/jeffknupp/sandman2/',
     license='Apache Software License',
     author='Jeff Knupp',
