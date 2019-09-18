77c3f4ca03beb52c2ad62d4b5ca04adfe87eebea
Carel van Dam <carelvdam@gmail.com>
2019-09-19 01:48:49 +0200

    Exposed the argument parser
    This simply moves the creation of the command line parser out of the main function so that others using the package might extend it.

--- flask_sandman/__main__.py
+++ flask_sandman/__main__.py
@@ -5,48 +5,46 @@ services automatically from existing databases."""
 import argparse
 from flask_sandman import create_app
 
+parser = argparse.ArgumentParser(
+    description='Auto-generate a RESTful API service '
+    'from an existing database.'
+    )
+parser.add_argument(
+    'URI',
+help='Database URI in the format DATABASE+PROTOCOL://USERNAME:PASSWORD@HOSTNAME:HOSTPORT/DATASTORE')
+parser.add_argument(
+    '-d',
+    '--debug',
+    help='Turn on debug logging',
+    action='store_true',
+    default=False)
+parser.add_argument(
+    '-p',
+    '--port',
+    help='Port for service to listen on',
+    default=5000)
+parser.add_argument(
+    '-l',
+    '--local-only',
+    help='Only provide service on localhost (will not be accessible'
+         ' from other machines)',
+    action='store_true',
+    default=False)
+parser.add_argument(
+    '-r',
+    '--read-only',
+    help='Make all database resources read-only (i.e. only the HTTP GET method is supported)',
+    action='store_true',
+    default=False)
+parser.add_argument(
+    '-s',
+    '--schema',
+    help='Use this named schema instead of default',
+    default=None)
 
-def main():
+
+def main(parser = parser):
     """Main entry point for script."""
-    parser = argparse.ArgumentParser(
-        description='Auto-generate a RESTful API service '
-        'from an existing database.'
-        )
-    parser.add_argument(
-        'URI',
-        help='Database URI in the format '
-             'postgresql+psycopg2://user:password@host/database')
-    parser.add_argument(
-        '-d',
-        '--debug',
-        help='Turn on debug logging',
-        action='store_true',
-        default=False)
-    parser.add_argument(
-        '-p',
-        '--port',
-        help='Port for service to listen on',
-        default=5000)
-    parser.add_argument(
-        '-l',
-        '--local-only',
-        help='Only provide service on localhost (will not be accessible'
-             ' from other machines)',
-        action='store_true',
-        default=False)
-    parser.add_argument(
-        '-r',
-        '--read-only',
-        help='Make all database resources read-only (i.e. only the HTTP GET method is supported)',
-        action='store_true',
-        default=False)
-    parser.add_argument(
-        '-s',
-        '--schema',
-        help='Use this named schema instead of default',
-        default=None)
-
-
     args = parser.parse_args()
     app = create_app(args.URI, read_only=args.read_only, schema=args.schema)
     if args.debug:
