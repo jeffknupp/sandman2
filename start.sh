06a8468ea71b42f70c990e2cca67a584947adb44
Maurits van der Schee <maurits@vdschee.nl>
2019-10-11 00:22:36 +0200

    Fix typo

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start sandman2
 
-if [ $DB_TYPE == "sqlite"" ] then
+if [ $DB_TYPE == "sqlite" ] then
     sandman2ctl -p $PORT $DB_TYPE+$DB_DRIVER:///$DATABASE
 else
     sandman2ctl -p $PORT $DB_TYPE+$DB_DRIVER://$USERNAME:$PASSWORD@$DB_NAME:$DB_PORT/$DATABASE
