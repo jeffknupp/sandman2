7a6fdb1a7b03b38b231a196bbeaa3d290341c3ec
Maurits van der Schee <maurits@vdschee.nl>
2019-10-11 01:13:13 +0200

    Fixing typos
    See #131

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -2,7 +2,7 @@
 
 # Start sandman2
 
-if [ $DB_TYPE == "sqlite" ]; then
+if [ "$DB_TYPE" = "sqlite" ]; then
     sandman2ctl $DB_TYPE+$DB_DRIVER:////$DATABASE
 else
     sandman2ctl $DB_TYPE+$DB_DRIVER://$USERNAME:$PASSWORD@$DB_HOST:$DB_PORT/$DATABASE
